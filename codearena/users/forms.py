25479572b236b988f00724d9d5038f1f1bf0362d
Suchith Sridhar <suchithsridhar@gmail.com>
2021-06-19 14:14:07 -0300

    Added other links and tags to user page

--- codearena/users/forms.py
+++ codearena/users/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from codearena.models import User
 
@@ -35,12 +35,13 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    skills = StringField('Skills')
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Update')
+    github = StringField('Github', validators=[])
+    linkedin = StringField('Linkedin', validators=[])
+    personal = StringField('Personal Domain', validators=[])
 
     def validate_username(self, username):
         if username.data != current_user.username:
@@ -53,4 +54,10 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
+
+    def validate_skills(self, skills):
+        pass
+        # if skills.data.strip() == "":
+        #     raise ValidationError('Cannot be empty.')
+
 
