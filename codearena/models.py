ee583d6c14fd6d5c2a336aa4ed067484730abc96
Suchith Sridhar <suchithsridhar@gmail.com>
2021-06-19 10:50:38 -0300

    made changes to models, updating with more data in tables and teams table

--- codearena/models.py
+++ codearena/models.py
@@ -11,14 +11,42 @@ def load_user(user_id):
 def create_uuid():
     return str(uuid4())
 
+team_members = db.table('team_members',
+    db.Column('team_id', db.String, db.ForeignKey('team.id'), nullable=False),
+    db.Column('user_id', db.String, db.ForeignKey('user.id'), nullable=False)
+)    
 
 class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key=True, default=create_uuid)
     username = db.Column(db.String(20), unique=True, nullable=False)
+    user_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    bio =  db.Column(db.Text)
+    github = db.Column(db.String(120), unique=False, nullable=True)
+    linkedin = db.Column(db.String(120), unique=False, nullable=True)
+    personal_site = db.Column(db.String(120), unique=False, nullable=True)
+    # team_invitations
+    # socail_network = db.column(db.Text)
+    skills = db.column(db.Text)
+    
 
     def __repr__(self):
         return f"User('{self.id}, {self.username}', '{self.email}', '{self.image_file}')"
+
+class Team(db.Model):
+    id = db.Column(db.String, primary_key=True, default=create_uuid)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    about = db.Column(db.Text)
+    # join_request = //check here
+    date_crated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    github = db.Column(db.String(120), unique=False, nullable=True)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    leader_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
+    team_members = db.relationship('user', secondary=team_members, lazy=True,
+        backref='team_members')
+
+    def __repr__(self):
+        return f"Team('{self.id}, {self.name}', '{self.about}', '{self.image_file}')"
 
